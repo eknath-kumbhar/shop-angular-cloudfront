394557f0b6643df824ddbaea80a12d996482d766
eknath-kumbhar <eknathkumbharv1@hotmail.com>
2022-10-31 15:10:10 +0530

    feat: task-5 enabled import service in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://k9817oo3ci.execute-api.ap-south-1.amazonaws.com/dev',
     bff: 'https://qaf1jha90a.execute-api.ap-south-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
